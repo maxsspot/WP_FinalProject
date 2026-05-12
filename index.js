000673cc75be77dca558e37c68ec857a9b056caf
Max <maxsnyder12345@gmail.com>
2026-05-12 12:19:13 -0400

    remove row of cells

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Calls functions at the beginning
 winStreakBar();
 makeWinningNumbers();
 makeTicket();
@@ -54,9 +55,12 @@ function assignEvents() {
     })
 }
 
+// Shows bottom bar of info
 function showInfo() {
     document.getElementById("info").style.bottom="0px"
 }
+
+// Hides bottom bar of info
 function hideInfo() {
     document.getElementById("info").style.bottom="-70px"
 }
@@ -66,18 +70,18 @@ function makeTicket() {
     // The collection that holds that values for each cell, the money gained, and the clicks left
     let cellNumbers=[]
     let moneyGained=[]
-    let clicksLeft=15
+    let clicksLeft=10
 
-    for(let i=0;i<=14;i++) {
+    for(let i=0;i<=9;i++) {
         // Defines the value a cell will have and creates the button
-        let numberToAdd=Math.floor(Math.random() * 60) + 1;
+        let numberToAdd=Math.floor(Math.random() * 50) + 1;
         let newCell=document.createElement("div");
         
         // Will eventually hold the winning cells
         let winningCells;
 
         // Gives an amount to each cell
-        let cellAmount=Math.floor(Math.random() * 10) + 1;
+        let cellAmount=Math.floor(Math.random() * 5) + 1;
 
         // Gives the button both an ID and a cell
         newCell.id=`cell${i}`;
@@ -105,7 +109,7 @@ function makeTicket() {
         // Ensures that the number that is assigned to a cell was not taken
         // then adds the number to the list of already taken numbers
         while(cellNumbers.includes(numberToAdd)) {
-            numberToAdd=Math.floor(Math.random() * 40) + 1;
+            numberToAdd=Math.floor(Math.random() * 50) + 1;
         }
         cellNumbers.push(numberToAdd)
         document.getElementById("cellZone").appendChild(newCell);
@@ -156,9 +160,9 @@ function makeWinningNumbers() {
     for(let i=0;i<4;i++) {
         let winningNum=document.createElement("p");
         winningNum.className="winningNumber";
-        winningNum.textContent=Math.floor(Math.random() * 60) + 1;
+        winningNum.textContent=Math.floor(Math.random() * 50) + 1;
         while(alreadyUsed.includes(winningNum.textContent)) {
-            winningNum.textContent=Math.floor(Math.random() * 60) + 1;
+            winningNum.textContent=Math.floor(Math.random() * 50) + 1;
         }
         alreadyUsed.push(winningNum.textContent)
         document.getElementById("winNumberZone").appendChild(winningNum);
